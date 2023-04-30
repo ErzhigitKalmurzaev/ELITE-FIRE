8efe8f96c69b94853497d0f8aed56d19f28f2375
Erzhigit <96490425+ErzhigitKalmurzaev@users.noreply.github.com>
2023-05-01 00:22:51 +0600

    last commit

--- js/script.js
+++ js/script.js
@@ -1,31 +1,88 @@
 function swagger(){
   const left = document.querySelectorAll(".swagger__left");
   const right = document.querySelectorAll(".swagger__right");
+  const block = document.querySelectorAll(".swagger__block");
+  const image = document.querySelector(".swagger__img");
+  let offset = 0;
 
-  left.forEach(item => {
-    item.addEventListener("click", (e) => {
-      e.preventDefault();
-      e.target.parentNode.parentNode.querySelectorAll(".swagger__img").forEach(item => {
-        if(item.classList.length > 1){
-          item.classList.remove('active');
-        }else{
-          item.classList.add('active');
-        }
-      })
+  left.forEach((item, i) => {
+    item.addEventListener("click", () => {
+      if(offset + image.offsetWidth >= block[i].offsetWidth){
+        offset = 0;
+      }else{
+        offset += image.offsetWidth;
+        console.log(image.offsetWidth, block[i].offsetWidth)
+      }
+      block[i].style.left = -offset + "px";
     })
   })
-  right.forEach(item=> {
-    item.addEventListener("click", (e) => {
-      e.preventDefault();
-      e.target.parentNode.parentNode.querySelectorAll(".swagger__img").forEach(item => {
-        if(item.classList.length > 1){
-          item.classList.remove('active');
-        }else{
-          item.classList.add('active');
-        }
-      })
+
+  right.forEach((item, i) => {
+    item.addEventListener("click", () => {
+      if(offset + image.offsetWidth >= block[i].offsetWidth){
+        offset = 0;
+      }else{
+        offset += image.offsetWidth;
+      }
+      console.log(offset)
+      block[i].style.left = -offset + "px";
     })
+  })
+
+}
+
+function slider(){
+  const left = document.querySelector(".slide__left");
+  const right = document.querySelector(".slide__right");
+  const block = document.querySelector(".slide__block");
+  const image = document.querySelector(".slide__img");
+  let offset = 0;
+
+  left.addEventListener("click", () => {
+    if(offset + image.offsetWidth >= block.offsetWidth){
+      offset = 0;
+    }else{
+      offset += image.offsetWidth;
+    }
+    block.style.left = -offset + "px";
+  })
+
+  right.addEventListener("click", () => {
+    if(offset + image.offsetWidth >= block.offsetWidth){
+      offset = 0;
+    }else{
+      offset += image.offsetWidth;
+    }
+    block.style.left = -offset + "px";
   })
 }
 
+function burger(){
+  const button = document.querySelector(".header__burger");
+  const modal = document.querySelector(".header__modal");
+  const close = document.querySelector(".close-button")
+
+  button.addEventListener('click', () => {
+    modal.style.transform = "translateY(-150%)";
+    modal.style.animation = "ani 0.3s forwards"
+    modal.style.display = 'flex';
+  })
+  close.addEventListener('click', () => {
+    modal.style.transform = "translateY(0)";
+    modal.style.animation = "ani2 0.5s forwards";
+  })
+  
+}
+
+function range(){
+  const input = document.querySelector('.range');
+  input.addEventListener('input', (e) => {
+    input.style.background = `linear-gradient(to right, rgba(255, 207, 122, 1) 0%, rgba(255, 207, 122, 1) ${e.target.value}%, rgba(148, 148, 148, 1) ${e.target.value}%, rgba(148, 148, 148, 1) 100%`;
+  })
+}
+
+range();
+burger();
+slider();
 swagger();
+
